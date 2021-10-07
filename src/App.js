6b5bba5cff4f70974108fa8836ee84b20ec8e245
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-07 18:53:57 +0600

    login start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import './App.css';
 import initializeAuthentication from './Firebase/firebase.initialize';
@@ -10,6 +10,8 @@ function App() {
   const auth = getAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isLogIn, setIsLogIn] = useState(false);
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, googleProvider)
@@ -20,6 +22,9 @@ function App() {
       .catch(error => {
         console.log(error.message);
       })
+  }
+  const toggleLogin = (e) => {
+    setIsLogIn(e.target.checked);
   }
 
 
@@ -32,40 +37,77 @@ function App() {
   }
 
   const handleSignUp = (e) => {
-    console.log(email, password);
     e.preventDefault();
+    if (password.length < 6) {
+      setError('Password must be at least 6 characters long');
+      return;
+    }
+    if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+      setError('Password must contain 2 upper letter');
+      return;
+    }
+    isLogIn ? processLogIn(email, password) : registerNewUser(email, password);
+  }
+  const processLogIn = (email, password) => {
+
+  }
+
+  const registerNewUser = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+
+        const user = result.user;
+        console.log(user);
+        setError('');
+      })
+
+      .catch(error => {
+        setError(error.message);
+      })
   }
 
   return (
     <div className='mx-5 my-5  '>
-      <h3 className='text-center text-primary mb-4'>Please Sign Up</h3>
+      <h3 className='text-center text-primary mb-4'>Please {(isLogIn) ? "Sign In" : 'Sign Up'}</h3>
       <form onSubmit={handleSignUp}>
         <div className="row mb-3 d-flex justify-content-center ">
           <label htmlFor="inputEmail3" className="col-sm-1 col-form-label"><strong>Email: </strong></label>
           <div className="col-sm-4">
-            <input onBlur={handleEmailChange} type="email" className="form-control" id="inputEmail3" />
+            <input onBlur={handleEmailChange} type="email" className="form-control" id="inputEmail3" required />
           </div>
         </div>
         <div className="row mb-3 d-flex justify-content-center">
           <label htmlFor="inputPassword3" className="col-sm-1 col-form-label"><strong>Password: </strong></label>
           <div className="col-sm-4">
-            <input onBlur={handlePasswordChange} type="password" className="form-control" id="inputPassword3" />
+            <input onBlur={handlePasswordChange} type="password" className="form-control" id="inputPassword3" required />
           </div>
         </div>
         <div className="row mb-3">
           <div className="col-sm-8 offset-sm-2  d-flex justify-content-center">
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" id="gridCheck1" />
+              <input onChange={toggleLogin} className="form-check-input" type="checkbox" id="gridCheck1" />
               <label className="form-check-label" htmlFor="gridCheck1">
-                Example checkbox
+                Already registered?
               </label>
             </div>
           </div>
         </div>
+        <div className="row mb-3 text-center text-danger">
+          <p>{error}</p>
+        </div>
         <div className=" d-flex justify-content-center">
-          <button type="submit" className="btn btn-primary  ">Sign in</button>
+          <button type="submit" className="btn btn-primary ">{(isLogIn) ? 'Sign In' : "Sign Up"}</button>
         </div>
       </form>
+
+      <div className='text-center'>
+        <br /><br /><br />
+        <div>--------------------------------</div>
+        <br /><br /><br />
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      </div>
+
     </div>
   );
 }
